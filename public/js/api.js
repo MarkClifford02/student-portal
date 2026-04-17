1fcc4ee9ac69a4ca6d02b351ec4625f518d8ede6
BernaldCatli <bernaldcatli@gmail.com>
2026-04-17 15:11:25 +0800

    Fix file upload: allow multipart/form-data for FormData bodies

--- public/js/api.js
+++ public/js/api.js
@@ -2,10 +2,16 @@ const API_BASE = '/api';
 
 async function fetchAPI(endpoint, options = {}) {
     try {
+        // Don't set Content-Type for FormData - let browser handle multipart/form-data
+        const headers = {};
+        if (!(options.body instanceof FormData)) {
+            headers['Content-Type'] = 'application/json';
+        }
+        
         const response = await fetch(`${API_BASE}${endpoint}`, {
             ...options,
             headers: {
-                'Content-Type': 'application/json',
+                ...headers,
                 ...options.headers
             }
         });
